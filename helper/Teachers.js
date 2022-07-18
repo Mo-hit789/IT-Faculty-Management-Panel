edbadfc61dea72f7f6b7bdc84b2b0a11e5dd9740
mohit vishwakarma <Mohitvishwakarma7891@gmail.com>
2022-07-18 14:23:56 +0530

    Update Teachers.js

--- helper/Teachers.js
+++ helper/Teachers.js
@@ -8,20 +8,20 @@ export const Teachers = [
         "course": "Computer Networks, Cyber Law and Forensics, Internet Technology and Network Management",
         "specialization": "Networking and Communication",
         "experience": "28",
-        "url": "http://localhost:3000/faculty/asmita",
+        "url": "/faculty/roopam",
         "research": [
             {
-                "title": "",
-                "journal": "",
-                "date": "",
+                "title": "Design and Development of an Efficient Network Intrusion Detection System Using Machine Learning Techniques",
+                "journal": "Wireless communications and mobile computing",
+                "date": "2021",
                 "vol": "",
                 "issn": "",
                 "doi": ""
             },
             {
-                "title": " ",
-                "journal": " ",
-                "date": " ",
+                "title": "An efficient feature subset selection approach for machine learning",
+                "journal": "GIJET",
+                "date": "2021",
                 "vol": "",
                 "issn": "",
                 "doi": ""
@@ -63,9 +63,10 @@ export const Teachers = [
         "course": "Theory of Computation, Information Security, Operating System",
         "specialization": "Digital watermarking",
         "experience": "16",
+        "url": "/faculty/sachin",
         "research": [
             {
-                "title": "An Enhancement of BAT Algorithm using Diversity function for Routing optimization in WSN",
+                "title": "An Optimized Communication Scheme for Energy Efficient and Secure Flying Ad-hoc Network (FANET)",
                 "journal": " International Journal of Control Theory and Applications ",
                 "date": "june 2017",
                 "vol": "10",
@@ -94,25 +95,26 @@ export const Teachers = [
 
     {
         "name": "Dr. Anjana Pandey",
-        "img": "https://photovideolounge.com/wp-content/uploads/2019/11/blonde-girl-in-blue-neutral-facial-expression-1.jpg",
+        "img": "https://drive.google.com/file/d/1qr4OM4i6syE7bqXog_8swnDAEy4yeogq/view?usp=sharing",
         "designation": "Associate Professor",
         "course": "Data Mining & Warehousing, E - Commerce  & E-Governance",
         "specialization": "Data Mining, Social Network",
         "experience": "16",
+        "url": "/faculty/Anjana",
         "research": [
             {
-                "title": "",
-                "journal": "",
-                "date": "",
+                "title": "PPCI Algorithm for Mining Temporal Association Rules In Large databases” ",
+                "journal": " Journal of Information & Knowledge Management",
+                "date": "2009",
                 "vol": "",
                 "issn": "",
                 "doi": ""
             },
             {
-                "title": " ",
-                "journal": " ",
-                "date": " ",
-                "vol": "",
+                "title": "A Survey on Community Detection Algorithm and Its Applications",
+                "journal": " “Turkish Journal of Computer and Mathematics Education",
+                "date": "2021",
+                "vol": "12",
                 "issn": "",
                 "doi": ""
             }
@@ -127,19 +129,20 @@ export const Teachers = [
         "course": "Digital Circuits & Systems, Computer Architecture",
         "specialization": "Digital Communication",
         "experience": "13",
+        "url": "/faculty/Ratish",
         "research": [
             {
-                "title": "",
-                "journal": "",
-                "date": "",
+                "title": "A BSA based Communication Strategy for Energy Efficient Flying Ad-hoc Network(FANET)",
+                "journal": "DesignEngineering",
+                "date": "2021",
                 "vol": "",
                 "issn": "",
                 "doi": ""
             },
             {
-                "title": " ",
-                "journal": " ",
-                "date": " ",
+                "title": "An Optimized Communication Scheme for Energy Efficient and Secure Flying Ad-hoc Network (FANET), ",
+                "journal": "Wireless Personal Communications.",
+                "date": "2021",
                 "vol": "",
                 "issn": "",
                 "doi": ""
@@ -154,20 +157,21 @@ export const Teachers = [
         "course": "Database Management System, Distributed system",
         "specialization": "Software Engineering, DBMS, Big Data and Hadoop",
         "experience": "20",
+        "url": "/faculty/mahesh",
         "research": [
             {
-                "title": "",
-                "journal": "",
-                "date": "",
-                "vol": "",
+                "title": "Performance Evaluation of Classification Algorithm J48 on Heart attack and its diseases",
+                "journal": "JETIR",
+                "date": "2018",
+                "vol": "5",
                 "issn": "",
                 "doi": ""
             },
             {
-                "title": " ",
-                "journal": " ",
-                "date": " ",
-                "vol": "",
+                "title": "Emotion Classification from EEG",
+                "journal": "JETIR",
+                "date": "2018",
+                "vol": "5",
                 "issn": "",
                 "doi": ""
             }
@@ -182,6 +186,7 @@ export const Teachers = [
         "course": " Data Structures, Analysis and Design of Algorithms, Fundamental of IT, Environmental Science",
         "specialization": "Image Processing",
         "experience": "8",
+        "url": "/faculty/neera",
         "research": [
             {
                 "title": "",
@@ -209,6 +214,7 @@ export const Teachers = [
         "course": "OOAD, JAVA, FOCSE, EOITK, Web Tech",
         "specialization": "Image Processing",
         "experience": "3.10",
+        "url": "/faculty/ekta",
         "research": [
             {
                 "title": "",
@@ -237,6 +243,7 @@ export const Teachers = [
         "course": "Operating System, Discrete Structures, Compiler Designh",
         "specialization": "Neural Network, IoT",
         "experience": "9.2",
+        "url": "/faculty/shifa",
         "research": [
             {
                 "title": "",
@@ -264,6 +271,7 @@ export const Teachers = [
         "course": "Fundamental of Computer Science & engg., OOAD, Artificial Intelligence, CSO, Microprocessor",
         "specialization": "Computer Network and Programming Language",
         "experience": "5",
+        "url": "/faculty/mahendra",
         "research": [
             {
                 "title": "",
@@ -291,6 +299,7 @@ export const Teachers = [
         "course": "Object Oriented Programming & Methodology, Computer Graphics & Multimedia, Artificial Intelligence, Soft Computing, Data Science, Cloud Computing",
         "specialization": "",
         "experience": "9",
+        "url": "/faculty/vikas",
         "research": [
             {
                 "title": "",
